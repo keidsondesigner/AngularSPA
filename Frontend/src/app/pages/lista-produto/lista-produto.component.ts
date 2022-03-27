d57e7c6274c673ea586b4ba8236ef75ca79ec9a3
keidsondesigner <keidsondesigner@gmail.com>
2022-03-27 13:29:03 -0400

    ajuste mobile lista produto

--- Frontend/src/app/pages/lista-produto/lista-produto.component.ts
+++ Frontend/src/app/pages/lista-produto/lista-produto.component.ts
@@ -3,12 +3,17 @@ import { Component, OnInit } from "@angular/core";
 import { IProduto } from "src/app/model/produto.model";
 import { ProdutosService } from "src/app/service/produtos.service";
 
+import { NgxSpinnerService } from "ngx-spinner";
+
 @Component({
   selector: "app-lista-produto",
   templateUrl: "./lista-produto.component.html"
 })
 export class ListaProdutoComponent implements OnInit {
-  constructor(private produtoService: ProdutosService) {}
+  constructor(
+    private produtoService: ProdutosService,
+    private spinner: NgxSpinnerService
+  ) {}
 
   public produtos: IProduto[] = [];
 
@@ -16,9 +21,16 @@ export class ListaProdutoComponent implements OnInit {
     this.produtoService.obterProdutos().subscribe(
       produtos => {
         this.produtos = produtos;
-        console.log(produtos);
       },
       error => console.log(error)
     );
+
+    /** spinner starts on init */
+    this.spinner.show();
+
+    setTimeout(() => {
+      /** spinner ends after 5 seconds */
+      this.spinner.hide();
+    }, 2000);
   }
 }
